89e79dc27543765ebe17c9e7498706be6f02bb61
Danny Thorne <dannythorne@gmail.com>
2012-03-12 18:14:53 -0400

    hands.cc -- initialize shoe

--- Hands/hands.cc
+++ Hands/hands.cc
@@ -2,10 +2,29 @@
 #include <iostream>
 using namespace std;
 
+const int N_CARD_TYPES = 10; // Number of card types: |{ 10, A, 2-9}| = 10
+
 int main()
 {
   cout << "Hello!" << endl;
-  // TODO
+
+  int num_decks = 6;
+  int shoe[N_CARD_TYPES];
+
+  int i;
+
+  for( i=0; i<N_CARD_TYPES; i++)
+  {
+    shoe[i] = num_decks*4;
+  }
+  shoe[0]*=4; // Four times as many tens.
+
+  cout << "In a shoe with " << num_decks << " decks:" << endl;
+  for( i=0; i<N_CARD_TYPES; i++)
+  {
+    cout << "  Card type " << i << " occurs " << shoe[i] << " times." << endl;
+  }
+
   cout << "Good bye!" << endl;
   return 0;
 }
