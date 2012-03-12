41de0aacb354aa59c544559c649536aa3f4b2996
Danny Thorne <dannythorne@gmail.com>
2012-03-12 16:26:32 -0400

    Starting hands.cc to generate all possible hands and associated probabilities.

--- btrials/btrials.cc
+++ btrials/btrials.cc
@@ -5,32 +5,33 @@
 #include <ctime>
 using namespace std;
 
-void play( const float p
-         , const float pbing
-         , const int num_trials
-         , const int base_wager
-         , const int max_wager
-         , int &num_wins
-         , int &winnings );
+struct BTrial
+{
+  float p;
+  float pbing;
+  int num_trials;
+  int max_trials;
+  int base_wager;
+  int max_wager;
+  int num_wins;
+  int winnings;
+};
+
+void init( BTrial& btrial);
+void play( BTrial& btrial);
 
 int main()
 {
   srand(time(0));
 
-  int num_trials;
-  int num_wins;
-  int winnings;
+  BTrial btrial;
+
+  init( btrial);
 
   float ave_wins;
   float ave_winnings;
   float ave_positive;
 
-  float p = 0.49;
-  float pbing = 0.04;
-
-  int max_trials = 3e3;
-  int base_wager = 1;
-  int max_wager = 128;
   int n = 1e3;
 
   int i;
@@ -38,7 +39,9 @@ int main()
   cout << "%% num_trials ave_wins ave_winnings ave_positive" << endl;
   cout << "x = [" << endl;
 
-  for( num_trials=1; num_trials<=max_trials; num_trials++)
+  for( btrial.num_trials=1;
+       btrial.num_trials<=btrial.max_trials;
+       btrial.num_trials++)
   {
     ave_wins = 0;
     ave_winnings = 0;
@@ -46,11 +49,11 @@ int main()
 
     for( i=0; i<n; i++)
     {
-      play( p, pbing, num_trials, base_wager, max_wager, num_wins, winnings);
+      play( btrial);
 
-      ave_wins += num_wins;
-      ave_winnings += winnings;
-      if( winnings > 0)
+      ave_wins += btrial.num_wins;
+      ave_winnings += btrial.winnings;
+      if( btrial.winnings > 0)
       {
         ave_positive+=1;
       }
@@ -60,9 +63,9 @@ int main()
     ave_winnings /= n;
     ave_positive /= n;
 
-    cout << setw(4) << num_trials
+    cout << setw(4) << btrial.num_trials
          << " "
-         << fixed << setprecision(8) << (double)ave_wins / num_trials
+         << fixed << setprecision(8) << (double)ave_wins / btrial.num_trials
          << " "
          << setw(5) << ave_winnings
          << " "
@@ -73,39 +76,44 @@ int main()
 
   cout << "];" << endl;
   cout << "figure; plot(x(:,end)); "
-       << "axis([ 0 " << num_trials << " 0 1]);" << endl;
+       << "axis([ 0 " << btrial.num_trials << " 0 1]);" << endl;
 
   return 0;
 }
 
-void play( const float p
-         , const float pbing
-         , const int num_trials
-         , const int base_wager
-         , const int max_wager
-         , int &num_wins
-         , int &winnings
-         )
+void init( BTrial& btrial)
 {
-  num_wins = 0;
-  int wager = base_wager;
-  winnings = 0;
+  btrial.p = 0.49;
+  btrial.pbing = 0.04;
+  btrial.max_trials = 1e3;
+  btrial.base_wager = 1;
+  btrial.max_wager = 128;
+}
+
+void play( BTrial& btrial)
+{
+  btrial.num_wins = 0;
+  int wager = btrial.base_wager;
+  btrial.winnings = 0;
 
   int i;
-  for( i=0; i<num_trials; i++)
+  for( i=0; i<btrial.num_trials; i++)
   {
-    if( (double)rand()/(double)RAND_MAX < p)
+    if( (double)rand()/(double)RAND_MAX < btrial.p)
     {
-      num_wins++;
-      winnings += wager;
-      if( (double)rand()/RAND_MAX < pbing) { winnings+=(wager/2);}
-      wager = base_wager;
+      btrial.num_wins++;
+      btrial.winnings += wager;
+      if( (double)rand()/RAND_MAX < btrial.pbing)
+      {
+        btrial.winnings+=(wager/2);
+      }
+      wager = btrial.base_wager;
     }
     else
     {
-      winnings -= wager;
+      btrial.winnings -= wager;
       wager*=2;
-      if( wager > max_wager) { wager = base_wager;}
+      if( wager > btrial.max_wager) { wager = btrial.base_wager;}
     }
   }
 }
