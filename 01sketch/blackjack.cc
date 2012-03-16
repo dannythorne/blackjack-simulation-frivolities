fe02b4b9049b825d0cb3d53d19c0ffb903b36188
Danny Thorne <dannythorne@gmail.com>
2012-03-16 00:44:38 -0400

    Player now keeps track of hand values. Added a Scribe class to handle keeping track of data.

--- 01sketch/blackjack.cc
+++ 01sketch/blackjack.cc
@@ -1,22 +1,26 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 using namespace std;
 
-void extract_args( int argc, char** argv, int& numRounds);
+void extract_args( const int argc, const char** argv, int& numRounds);
 
 class Shoe;
 
 class Rules
 {
 public:
-  Rules( double max_penetration_ratio=0.7) { m_max_penetration_ratio = max_penetration_ratio;}
+  Rules( const double max_penetration_ratio=0.7)
+  {
+    m_max_penetration_ratio = max_penetration_ratio;
+  }
 
-  bool allow_splitting();
-  bool allow_doubling_down();
-  bool allow_doubling_down_after_splitting();
-  bool say_it_is_time_to_reset_the( Shoe& shoe);
-  double max_penetration_ratio() { return m_max_penetration_ratio;}
+  bool allow_splitting() const;
+  bool allow_doubling_down() const;
+  bool allow_doubling_down_after_splitting() const;
+  bool say_it_is_time_to_reset_the( const Shoe& shoe) const;
+  double max_penetration_ratio() const { return m_max_penetration_ratio;}
 
 private:
   double m_max_penetration_ratio;
@@ -26,7 +30,9 @@ private:
 class Player
 {
 public:
-  Player() { m_num_hands = 1;}
+  Player( const Rules& rules) : m_rules(rules) { m_num_hands = 1;}
+
+  void init( const int round);
 
   bool has_blackjack();
   bool should_split();
@@ -35,11 +41,13 @@ public:
 
   int num_hands() { return m_num_hands;}
 
-  void first_hand() { m_hand = 1;}
+  void first_hand() { m_hand = 0;}
   bool has_more_hands() { return m_hand<m_num_hands;}
   void next_hand() { m_hand++;}
 
-  void sees( int dealer_up_card) { m_dealer_up_card = dealer_up_card;}
+  void sees( const int dealer_up_card) { m_dealer_up_card = dealer_up_card;}
+
+  void dealt( const int next_card);
 
   void split();
 
@@ -48,7 +56,13 @@ private:
   int m_hand;
   int m_num_hands;
   int m_num_cards[4];
+
+  int m_hand_value[4];
+  int m_num_elevens[4];
+
   int m_dealer_up_card;
+
+  const Rules& m_rules;
 
 };
 
@@ -56,7 +70,12 @@ class Dealer
 {
 public:
   Dealer() {}
+
+  void init( const int round);
+
   bool has_blackjack();
+  bool should_hit();
+
   int up_card();
 
 private:
@@ -77,47 +96,64 @@ public:
 
   void reset();
 
+  int next_card();
+
   void deal_to( Player& player);
   void deal_to( Dealer& dealer);
 
-  int penetration() { return m_penetration;}
-  double penetration_ratio();
+  int penetration() const { return m_penetration;}
+  double penetration_ratio() const;
 
 private:
   int m_cards[10]; // { |10s|, |As|, |2s|, ..., |9s|}
   int m_num_decks;
   int m_num_cards_total;
-  int m_penetration;
+  int m_penetration; // number of cards dealt
+
+};
+
+class Scribe
+{
+public:
+  Scribe() {}
+
+  void init( const int round) {}
+
+private:
 
 };
 
 class Message
 {
 public:
-  Message( Rules& rules
-         , Shoe& shoe
-         , Player& player
-         , Dealer& dealer)
+  Message( const Rules& rules
+         , const Shoe& shoe
+         , const Player& player
+         , const Dealer& dealer
+         , const Scribe& scribe
+         )
   : m_rules(rules)
   , m_shoe(shoe)
   , m_player(player)
   , m_dealer(dealer)
+  , m_scribe(scribe)
   {}
 
-  void at_beginning_of( int round);
-  void at_end_of( int round);
+  void at_beginning_of( int round) const;
+  void at_end_of( int round) const;
 
 private:
-  Rules& m_rules;
-  Shoe& m_shoe;
-  Player& m_player;
-  Dealer& m_dealer;
+  const Rules& m_rules;
+  const Shoe& m_shoe;
+  const Player& m_player;
+  const Dealer& m_dealer;
+  const Scribe& m_scribe;
 
 };
 
 //##############################################################################
 
-int main( int argc, char** argv)
+int main( const int argc, const char** argv)
 {
   cout << __FILE__ << " " << __LINE__ << " -- " << "Hello." << endl;
 
@@ -129,10 +165,12 @@ int main( int argc, char** argv)
 
   Shoe shoe;
 
-  Player player;
+  Player player(rules);
   Dealer dealer;
 
-  Message message(rules,shoe,player,dealer);
+  Scribe scribe;
+
+  Message message(rules,shoe,player,dealer,scribe);
 
   int i;
 
@@ -141,6 +179,10 @@ int main( int argc, char** argv)
   for( round=1; round<=numRounds; round++)
   {
     message.at_beginning_of( round);
+
+    player.init( round);
+    dealer.init( round);
+    scribe.init( round);
 
     shoe.deal_to( player);
     shoe.deal_to( dealer);
@@ -170,12 +212,7 @@ int main( int argc, char** argv)
       //
       for( player.first_hand(); player.has_more_hands(); player.next_hand())
       {
-        if(  (   (   rules.allow_doubling_down())
-              ||
-                 (   player.num_hands()>1
-                  && rules.allow_doubling_down_after_splitting())
-             )
-           && player.should_double_down())
+        if( player.should_double_down())
         {
         }
         else
@@ -187,10 +224,15 @@ int main( int argc, char** argv)
           {
             while( player.should_hit())
             {
+              shoe.deal_to( player);
             }
           }
         }
+      }
 
+      while( dealer.should_hit())
+      {
+        shoe.deal_to( dealer);
       }
 
     }
@@ -201,28 +243,28 @@ int main( int argc, char** argv)
 
   }
 
-  cout << __FILE__ << " " << __LINE__ << " -- " << "Good Bye." << endl;
+  cout << endl << __FILE__ << " " << __LINE__ << " -- " << "Good Bye." << endl;
   return 0;
 }
 
 //##############################################################################
 
-bool Rules::allow_splitting()
+bool Rules::allow_splitting() const
 {
   return true;
 }
 
-bool Rules::allow_doubling_down()
+bool Rules::allow_doubling_down() const
 {
   return true;
 }
 
-bool Rules::allow_doubling_down_after_splitting()
+bool Rules::allow_doubling_down_after_splitting() const
 {
   return true;
 }
 
-bool Rules::say_it_is_time_to_reset_the( Shoe& shoe)
+bool Rules::say_it_is_time_to_reset_the( const Shoe& shoe) const
 {
   return shoe.penetration_ratio() >= max_penetration_ratio();
 }
@@ -243,13 +285,46 @@ bool Dealer::has_blackjack()
 
 //------------------------------------------------------------------------------
 
+void Dealer::init( const int round)
+{
+  m_num_cards = 0;
+}
+
 int Dealer::up_card()
 {
   // TODO
   return 0;
 }
 
+bool Dealer::should_hit()
+{
+  // TODO
+  return false;
+}
+
 //------------------------------------------------------------------------------
+
+void Player::init( const int round)
+{
+  m_hand = 0;
+  m_num_hands = 1;
+
+  m_num_cards[0] = 0;
+  m_num_cards[1] = 0;
+  m_num_cards[2] = 0;
+  m_num_cards[3] = 0;
+
+  m_hand_value[0] = 0;
+  m_hand_value[1] = 0;
+  m_hand_value[2] = 0;
+  m_hand_value[3] = 0;
+
+  m_num_elevens[0] = 0;
+  m_num_elevens[1] = 0;
+  m_num_elevens[2] = 0;
+  m_num_elevens[3] = 0;
+
+}
 
 bool Player::should_split()
 {
@@ -259,19 +334,53 @@ bool Player::should_split()
 
 bool Player::should_double_down()
 {
+  bool val = false;
+  if(   ( m_rules.allow_doubling_down())
+      ||
+        ( num_hands()>1 && m_rules.allow_doubling_down_after_splitting())
+    )
+  {
   // TODO
-  return false;
+  }
+  return val;
 }
 
 bool Player::should_hit()
 {
-  // TODO
+  int hand_val = m_hand_value[m_hand];
+  if( hand_val < 17) { return true;}
   return false;
 }
 
 void Player::split()
 {
   // TODO
+}
+
+void Player::dealt( const int next_card)
+{
+  m_hands[m_hand][m_num_cards[m_hand]] = next_card;
+  m_num_cards[m_hand]++;
+
+  if( next_card==0)
+  {
+    m_hand_value[m_hand] += 10;
+  }
+  else if( next_card==1)
+  {
+    m_hand_value[m_hand] += 11;
+    m_num_elevens[m_hand]++;
+  }
+  else
+  {
+    m_hand_value[m_hand] += next_card;
+  }
+
+  while( m_hand_value[m_hand] > 21 && m_num_elevens[m_hand] > 0)
+  {
+    m_hand_value[m_hand]-=10;
+    m_num_elevens[m_hand]--;
+  }
 }
 
 //------------------------------------------------------------------------------
@@ -288,9 +397,26 @@ void Shoe::reset()
   m_penetration = 0;
 }
 
+int Shoe::next_card()
+{
+  int n;
+
+  do
+  {
+    n = 1+(int)floor(13*((double)rand()/RAND_MAX));
+    if( n > 9) { n = 0;}
+
+  } while( !m_cards[n]);
+
+  m_cards[n]--;
+
+  return n;
+}
+
 void Shoe::deal_to( Player& player)
 {
-  // TODO
+  player.dealt( next_card());
+
   m_penetration++;
 }
 
@@ -300,16 +426,16 @@ void Shoe::deal_to( Dealer& dealer)
   m_penetration++;
 }
 
-double Shoe::penetration_ratio()
+double Shoe::penetration_ratio() const
 {
   return (double)m_penetration / m_num_cards_total;
 }
 
-void Message::at_beginning_of( int round)
+void Message::at_beginning_of( int round) const
 {
   cout << endl << "Round " << round << ":" << endl;
 }
-void Message::at_end_of( int round)
+void Message::at_end_of( int round) const
 {
   cout << "  end of round penetration"
        << ": " << m_shoe.penetration()
@@ -319,7 +445,7 @@ void Message::at_end_of( int round)
 
 //##############################################################################
 
-void extract_args( int argc, char** argv, int& numRounds)
+void extract_args( const int argc, const char** argv, int& numRounds)
 {
   int n;
   switch( argc)
